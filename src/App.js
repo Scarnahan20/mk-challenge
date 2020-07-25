93930db48cc692bfa4e3dd338a54eea369ff61e1
Scarnahan20 <55592707+Scarnahan20@users.noreply.github.com>
2020-07-25 11:13:20 -0700

    Continuing validation logic

--- src/App.js
+++ src/App.js
@@ -1,187 +1,194 @@
 import React, { useState } from 'react';
-import { Button, FormControl, TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+
 function App() {
-  const [errors, setErrors] = useState({
-    nameError: false,
-    emailError: false,
-    messageError: false,
-    passwordError: false
+  const [name, setName] = useState({
+    error: true,
+    text: "",
+    helper: ""
+  });
+  const [email, setEmail] = useState({
+    error: true,
+    text: "",
+    helper: ""
+  });
+  const [message, setMessage] = useState({
+    error: true,
+    text: "",
+    helper: ""
+  });
+  const [password, setPassword] = useState({
+    error: true,
+    text: "",
+    helper: ""
   });
 
-  const [nameText, setNameText] = useState("");
-  const [nameHelper, setNameHelper] = useState("");
-  const [emailText, setEmailText] = useState("");
-  const [emailHelper, setEmailHelper] = useState("");
-  const [messageText, setMessageText] = useState("");
-  const [messageHelper, setMessageHelper] = useState("");
-  const [passwordText, setPasswordText] = useState("");
-  const [passwordHelper, setPasswordHelper] = useState("");
-
-  function handleSubmit() {
+  function handleSubmit(event) {
+    event.preventDefault()
     if(checkFields) {
-      //submit
+      
     }
   }
 
   function checkFields(){
-    if (errors.nameError && errors.emailError && errors.messageError && errors.passwordError){
+    
+    if (name.error && email.error && message.error && password.error){
       return true;
     } else {
       return false;
     }
   }
 
-  function handleName(event) {
+  function handleChange(event) {
+    const fieldName = event.target.name;
     const currValue = event.target.value;
-    setNameText(currValue);
+    if (fieldName === 'name'){
+      setName(prevValue => {
+        return {
+          ...prevValue,
+          text: currValue
+        };
+      });
 
-    if(currValue === ''){
-      setErrors(prevValue => {
+      
+    } else if (fieldName === 'email') {
+      setEmail(prevValue => {
         return {
           ...prevValue,
-          nameError: true
+          text: currValue
         };
       });
-      setNameHelper("Name is required.");
-    } else {
-      setErrors(prevValue => {
-        return {
-          ...prevValue,
-          nameError: false
-        };
-      });
-      setNameHelper("");
-    }
-  }
 
-  function handleEmail(event) {
-    const currValue = event.target.value;
-    setEmailText(currValue);
+      if (email.text.trim() === ''){
+        setEmail(prevValue => {
+          return {
+            ...prevValue,
+            error: true,
+            helper: 'Email is required.'
+          };
+        });
+      } else {
+        setEmail(prevValue => {
+          return {
+            ...prevValue,
+            error: false,
+            helper: ''
+          };
+        });
+      }
+    } else if (fieldName === 'message') {
+      setMessage(prevValue => {
+        return {
+          ...prevValue,
+          text: currValue
+        };
+      });
 
-    if(currValue === ''){
-      setErrors(prevValue => {
+      if (message.text.trim() === ''){
+        setMessage(prevValue => {
+          return {
+            ...prevValue,
+            error: true,
+            helper: 'Message is required.'
+          };
+        });
+      } else {
+        setMessage(prevValue => {
+          return {
+            ...prevValue,
+            error: false,
+            helper: ''
+          };
+        });
+      }
+    } else if (fieldName === 'password'){
+      setPassword(prevValue => {
         return {
           ...prevValue,
-          emailError: true
+          text: currValue
         };
       });
-      setEmailHelper("Email is required.");
-    } else {
-      setErrors(prevValue => {
-        return {
-          ...prevValue,
-          emailError: false
-        };
-      });
-      setEmailHelper("");
-    }
-  }
 
-  function handleMessage(event) {
-    const currValue = event.target.value;
-    setMessageText(currValue);
-
-    if(currValue === ''){
-      setErrors(prevValue => {
-        return {
-          ...prevValue,
-          messageError: true
-        };
-      });
-      setMessageHelper("Message is required.");
-    } else {
-      setErrors(prevValue => {
-        return {
-          ...prevValue,
-          messageError: false
-        };
-      });
-      setMessageHelper("");
-    }
-  }
-
-  function handlePassword(event) {
-    const currValue = event.target.value;
-    setPasswordText(currValue);
-
-    if(currValue === ''){
-      setErrors(prevValue => {
-        return {
-          ...prevValue,
-          passwordError: true
-        };
-      });
-      setPasswordHelper("Password is required.");
-    } else {
-      setErrors(prevValue => {
-        return {
-          ...prevValue,
-          passwordError: false
-        };
-      });
-      setPasswordHelper("");
+      if (password.text.trim() === ''){
+        setPassword(prevValue => {
+          return {
+            ...prevValue,
+            error: true,
+            helper: 'Password is required.'
+          };
+        });
+      } else {
+        setPassword(prevValue => {
+          return {
+            ...prevValue,
+            error: false,
+            helper: ''
+          };
+        });
+      }
     }
   }
 
   return (
     <div className="App">
-      <FormControl onSubmit={handleSubmit} >
+      <form onSubmit={handleSubmit} >
         <div>
           <TextField
-            error={errors.nameError}
-            helperText={nameHelper}
-            id="name"
+            error={name.error}
+            helperText={name.helper}
+            name="name"
             label="Name"
-            onChange={handleName}
-            value={nameText}
+            onChange={handleChange}
+            value={name.text}
             variant="filled"
           />
         </div>
         <div>
           <TextField
-            error={errors.emailError}
-            helperText={emailHelper}
-            id="email"
+            error={email.error}
+            helperText={email.helper}
+            name="email"
             label="Email"
-            onChange={handleEmail}
+            onChange={handleChange}
             style={{margin: 4}}
-            value={emailText}
+            value={email.text}
             variant="filled"
           />
         </div>
         <div>
           <TextField 
-            error={errors.messageError}
-            helperText={messageHelper}
-            id="message"
+            error={message.error}
+            helperText={message.helper}
+            name="message"
             label="Message"
             multiline
-            onChange={handleMessage}
+            onChange={handleChange}
             rows="4"
             style={{margin: 4}}
-            value={messageText}
+            value={message.text}
             variant="filled"
           />
         </div>
         <div>
           <TextField
-            error={errors.passwordError}
-            helperText={passwordHelper}
-            id="password"
+            error={password.error}
+            helperText={password.helper}
+            name="password"
             label="Password"
-            onChange={handlePassword}
+            onChange={handleChange}
             style={{margin: 4}}
             type="password"
-            value={passwordText}
+            value={password.text}
             variant="filled"
           />
         </div>
-        <Button onClick={handleSubmit} variant="contained" color="primary">
-          Login
-        </Button>
-      </FormControl>
+          <Button color="primary" type="submit" variant="contained">
+            Submit
+          </Button>
+      </form>
     </div>
   );
 }
