3661678b324e519fb7d06713a95f10fe19a71c53
Scarnahan20 <55592707+Scarnahan20@users.noreply.github.com>
2020-07-26 11:00:23 -0700

    Rolled back backend

---
+++ build/precache-manifest.89d00454086cce507b95ca44a2500f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f363a5e5dacc8347ce256f458a875ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "339b8cc0aa2b390ea3c3",
+    "url": "/static/css/main.1ed90f8b.chunk.css"
+  },
+  {
+    "revision": "5e9d4f07019461eab6ef",
+    "url": "/static/js/2.b5569b10.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b5569b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339b8cc0aa2b390ea3c3",
+    "url": "/static/js/main.40eb1154.chunk.js"
+  },
+  {
+    "revision": "3c5d0e608c1d7d9b0dc2",
+    "url": "/static/js/runtime-main.0d65e27b.js"
+  }
+]);
